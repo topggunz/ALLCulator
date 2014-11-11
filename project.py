9b3ed88cbe670d695543d5abca14fff80941d538
Nathawut Worakijlawan <s7070038@kmitl.ac.th>
2014-11-12 00:37:53 +0700

    Try Build Frame Program.

--- project.py
+++ project.py
@@ -1,4 +1,27 @@
 """Project PSIT Startup at 11/11/2557
 Author : Nathawut Worakijlawan 
 """
-print "Hello Project"
+from tkinter import *
+
+class mainWindow(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+
+        self.master.title("MathPro Alpha Version (build 00011) **Name non official**")
+        self.master.geometry("840x550")
+
+        self.menubar = Menu(self, tearoff=False)
+        self.filemenu = Menu(self.menubar, tearoff=False)
+
+        
+        self.menubar.add_cascade(menu=self.filemenu, label="File")
+        self.menubar.add_cascade(menu=self.filemenu, label="Options")
+        self.menubar.add_cascade(menu=self.filemenu, label="Help")
+        
+        self.master.config(menu=self.menubar)
+        self.pack()
+        
+root = Tk()
+windows = mainWindow(root)
+root.mainloop()
+
