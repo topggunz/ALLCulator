751e7f41f909d2498d8e61c51d676459a3dec315
Nathawut Worakijlawan <s7070038@kmitl.ac.th>
2014-11-12 16:19:11 +0700

    Add About in Help menu

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 """Project PSIT Startup at 11/11/2557
 Author : Nathawut Worakijlawan 
 """
-from Tkinter import *
+from tkinter import *
 
 class mainWindow(Frame):
     def __init__(self, master):
@@ -11,16 +11,31 @@ class mainWindow(Frame):
         self.master.geometry("840x550")
 
         self.menubar = Menu(self, tearoff=False)
-        self.filemenu = Menu(self.menubar, tearoff=False)
+        self.filemenu = Menu(self.menubar, tearoff=0)
+        self.helpmenu = Menu(self.menubar, tearoff=0)
+        self.helpmenu.add_command(label="About", command=popup_about)
 
         
         self.menubar.add_cascade(menu=self.filemenu, label="File")
         self.menubar.add_cascade(menu=self.filemenu, label="Options")
-        self.menubar.add_cascade(menu=self.filemenu, label="Help")
+        self.menubar.add_cascade(menu=self.helpmenu, label="Help")
         
         self.master.config(menu=self.menubar)
         self.pack()
-        #bymimimimi
+        
+def popup_about():
+    top = Toplevel()
+    top.title("About MathPro")
+    top.geometry("250x300")
+
+    about_message = 'Hello World'
+
+    msg = Message(top, text=about_message)
+    msg.pack()
+
+    button = Button(top, text="Close", command=top.destroy)
+    button.pack()   
+
 root = Tk()
 windows = mainWindow(root)
 root.mainloop()
