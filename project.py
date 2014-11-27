5dee497f45001dde5167c413131c3a99490dd29b
mimiiiz <s7070145@kmitl.ac.th>
2014-11-27 22:46:53 +0700

    remove geometry set

--- project.py
+++ project.py
@@ -23,7 +23,7 @@ class mainWindow(Frame):
 
         #menu bar
         self.master.title("All Math (Alltimate Math)")
-        self.master.geometry("840x550")
+        #self.master.geometry("840x550")
         self.menubar = Menu(self, tearoff=False)
         self.optionmenu = Menu(self.menubar, tearoff=0)
         self.filemenu = Menu(self.menubar, tearoff=0)
@@ -82,14 +82,13 @@ class mainWindow(Frame):
         connect = mainConnect.cal_api(self.text_input.get())
 
     def widgets_input(self):
-        ####bymimimimi###
         #label for username
         self.username_input = StringVar()
         self.frame_username = ttk.LabelFrame(self.data_frm1, text='USER NAME',padding=5)
         #self.frame_username.pack(padx=10, pady=10)   
-        self.frame_username.grid(row=1, column=0,padx=10, pady=10)      
+        self.frame_username.grid(row=1, column=0, padx=10, pady=10, sticky=N+W)      
         self.label_username_input = Entry(self.frame_username, width=20, textvariable = self.username_input)
-        self.label_username_input.grid(row=1, column=0)
+        self.label_username_input.grid(row=1, column=0, sticky=N+W)
         #self.label_username_input.pack()
 
         #label for input
@@ -103,7 +102,7 @@ class mainWindow(Frame):
         
         #checkbutton for output
         self.option_frame = ttk.Labelframe(self.data_frm1, text='Select Output', padding=5)
-        self.option_frame.grid(row=3, column=0)
+        self.option_frame.grid(row=3, column=0, padx=5, pady=5)
         #self.option_frame.pack(padx=5, pady=5)
 
         self.output_tp = [
