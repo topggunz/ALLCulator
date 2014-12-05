ec75f840a6bd1147b583a72b847aac4686307012
mimiiiz <s7070145@kmitl.ac.th>
2014-12-05 23:27:08 +0700

    Debug, organize code
    Without bug where Output frame before reset
    insert list(self.storage = [ ]) to check have widgets yet.
    if yes then destroy and create new one
    ***********

--- project.py
+++ project.py
@@ -27,7 +27,6 @@ class Windows(Frame):
         Frame.__init__(self, master)
         #menu bar
         self.master.title("AllCulator (build 1)")
-        #self.master.geometry("550x550")
         self.menubar = Menu(self, tearoff=False)
         self.optionmenu = Menu(self.menubar, tearoff=0)
         self.filemenu = Menu(self.menubar, tearoff=0)
@@ -38,62 +37,61 @@ class Windows(Frame):
         self.menubar.add_cascade(menu=self.filemenu, label="File")
         self.menubar.add_cascade(menu=self.helpmenu, label="Help")
         self.master.config(menu=self.menubar)
-        self.pack()
-        #Frame input
-        self.content = Frame(master ,width=300,height=400,borderwidth=2,relief="groove")
-        self.content.pack()
+        self.grid(column=0, row=0)
+        #content
+        self.content = Frame(master, borderwidth=2, relief="groove")
+        self.content.grid(column=0, row=0)
+        #LOGO
+        canvas = Canvas(self.content,width=300,height=80,background="Black" )
+        canvas.grid(column=1, row=0, padx=10)
+        #Input Area
         self.note = ttk.Notebook(self.content,padding=2)
         self.data_frm1 = Frame(self.content,width=300,height=400,borderwidth=3,\
                 relief="ridge",padx=2,pady=2)
-        self.data_frm1.pack()
+        self.data_frm1.grid(column=0, row=0)
         self.note.add(self.data_frm1,text="Input Area",padding=5)
-        self.note.grid(column=0,row=0,rowspan=2,padx=5,pady=5)
-        self.widgets_input()
-
-    def cb_var(self):
-        '''for get value to radiobutton'''
-        index = self.v.get()
-        return index
-
-    def submit(self):
-        '''get input'''   
-        self.URL2 = mainConnect.call_api(self.text_input.get(), 'src')[self.cb_var()]
-        self.text = mainConnect.call_api(self.text_input.get(), 'alt')[self.cb_var()]
-        self.widgets_output(self.URL2, self.text)
-
-    def widgets_input(self):
-        '''Creat Input Widgets'''
-        #label for username
+        self.note.grid(column=0, row=0, rowspan=2, padx=5, pady=5)
+        #-label for username
         self.username_input = StringVar()
         self.frame_username = ttk.LabelFrame(self.data_frm1, text='USER NAME',padding=5)
-        self.frame_username.grid(row=1, column=0, padx=10, pady=10, sticky=N+W)      
+        self.frame_username.grid(column=0, row=0, padx=10, pady=10, sticky=N+W)      
         self.label_username_input = Entry(self.frame_username, width=20, textvariable = self.username_input)
-        self.label_username_input.grid(row=1, column=0, sticky=N+W)
-
-        #label for input
+        self.label_username_input.grid(column=0, row=0, sticky=N+W)
+        #-label for input
         self.text_input = StringVar()
         self.frame_input = ttk.LabelFrame(self.data_frm1, text='Enter what you want to calculate', padding=5)
-        self.frame_input.grid(row=2, column=0, padx=10, pady=10)
+        self.frame_input.grid(column=0, row=1, padx=10, pady=10)
         self.label_frame_input = Entry(self.frame_input, width=59, textvariable = self.text_input)
-        self.label_frame_input.grid(row=2, column=0)
-
-        #button
-        self.button_frame0 = Frame(self.data_frm1, height=2, bd=1, relief=SUNKEN)
-        self.button_frame0.grid(row=8, column=0, padx=5, pady=5)
-        self.b_genradio = Button(self.button_frame0, text="Submit", command=self.radiobutton, padx=5, pady=2).grid(column=0, row=0)
+        self.label_frame_input.grid(column=0, row=0)
+        #-button
+        self.button_frame0 = Frame(self.data_frm1, bd=1, relief=SUNKEN)
+        self.button_frame0.grid(column=0, row=2, padx=5, pady=5)
+        self.b_genradio = Button(self.button_frame0, text="Submit", padx=5, pady=2, command=self.radiobutton)# 
+        self.b_genradio.grid(column=0, row=0)
         
-        #LOGO
-        canvas = Canvas(self.content,width=300,height=80,background="Black" )
-        canvas.grid(row=0, column=1)
+        self.storage = []
+
+
+    def select_output(self):
+        '''get input and show output'''
+        index = self.v.get()
+        self.URL2 = mainConnect.call_api(self.text_input.get(), 'src')[index]
+        self.text = mainConnect.call_api(self.text_input.get(), 'alt')[index]
+        if len(self.storage) != 0:
+            self.ls_widgets = [self.note2, self.data_frm2, self.data_frm3]
+            for wid in self.ls_widgets:
+                wid.grid_remove()
+        self.widgets_output(self.URL2, self.text)
 
     def radiobutton(self):
         '''Creat Radiobutton , SelectOutput Button and Reset Button'''
         #checkbutton for output
         self.option_frame = ttk.Labelframe(self.data_frm1, text='Select Output', padding=5)
-        self.option_frame.grid(row=9, column=0, padx=5, pady=5)
+        self.option_frame.grid(column=0, row=3, padx=5, pady=5)
+        #generate from API
         input = self.text_input.get()
         print 'equation', input
-        self.pod = mainConnect.call_api(self.text_input.get(), 'pod') 
+        self.pod = Connect().call_api(self.text_input.get(), 'pod') 
         self.output_tp = []
         count = 0
         for name in self.pod:
@@ -110,21 +108,24 @@ class Windows(Frame):
             self.row += 1
 
         self.button_frame = Frame(self.data_frm1, height=2, bd=1, relief=SUNKEN)
-        self.button_frame.grid(row=10, column=0, padx=5, pady=5)
-        self.b_submit = Button(self.button_frame, text="Select Output", command=self.submit, padx=5, pady=2).grid(column=0, row=0)
-        self.b_reset = Button(self.button_frame, text="Reset", command=self.reset, padx=5, pady=2).grid(column=1, row=0)
+        self.button_frame.grid(column=0, row=5, padx=5, pady=5)
+        self.b_select_output = Button(self.button_frame, text="Select Output", command=self.select_output, padx=5, pady=2)
+        self.b_select_output.grid(column=0, row=0)
+        self.b_reset = Button(self.button_frame, text="Reset", command=self.reset, padx=5, pady=2)
+        self.b_reset.grid(column=1, row=0)
  
     def widgets_output(self, url, text):
         '''To Generate the Content for the Picture Frame'''
         #Frame output'''
-        self.note2 = ttk.Notebook(self.content,padding=2)
-        #   URL from API       
+        self.note2 = ttk.Notebook(self.content, padding=2)
+         #   URL from API       
         self.URL = url
         self.link = urllib.urlopen(self.URL)
         self.raw_data = self.link.read()
         self.link.close()
         next = base64.encodestring(self.raw_data)
         self.image = PhotoImage(data=next)
+        #widgets
         self.data_frm2 = Label(self.content,width=300,height=270,borderwidth=3,\
                 relief="ridge",padx=2,pady=2, image=self.image, bg='white')
         self.data_frm2.grid(column=0, row=1)
@@ -139,6 +140,8 @@ class Windows(Frame):
         self.note2.grid(column=1,row=0,rowspan=2,padx=5,pady=5)
         self.note2.add(self.data_frm3,text="Text Output",padding=5)
         self.note2.grid(column=1,row=1,rowspan=2,padx=5,pady=5)
+
+        self.storage.append('widgets')
 
     def reset(self):
         '''clear all of Entry and remove widgets output'''
@@ -237,6 +240,7 @@ def popup_about():
 
 def saveimage():
     pass
+
 root = Tk()
 windows = Windows(root)
 mainConnect = Connect()
